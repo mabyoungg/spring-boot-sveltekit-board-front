da8f3818b5e8be13f6076fe7b439c2ef9f0d8ae8
mabyoungg <mabyoungg@gmail.com>
2024-05-07 20:12:53 +0900

    feat: daisyUI 적용

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <div class="bg-red-500 w-[100px] h-[100px]">test</div>
+<button class="btn btn-primary">버튼</button>

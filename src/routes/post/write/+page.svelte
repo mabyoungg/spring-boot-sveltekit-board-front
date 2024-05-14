e271f5148b5960852a881a0c52e9a36477f30190
mabyoungg <mabyoungg@gmail.com>
2024-05-14 18:13:44 +0900

    feat: 글 작성 페이지

---
+++ src/routes/post/write/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Editor from '@toast-ui/editor';
+	import '@toast-ui/editor/dist/toastui-editor.css';
+
+	let div: HTMLDivElement;
+    
+	$effect(() => {
+		const editor = new Editor({
+			el: div,
+			height: 'calc(100dvh - 55px)',
+			initialEditType: 'markdown',
+			previewStyle: 'vertical'
+		});
+		return () => {
+			editor.destroy();
+		};
+	});
+</script>
+
+<div bind:this={div}></div>
